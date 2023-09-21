784b657d8c885e549ca19dc1e42d60ddfcc3df30
mdouglas <work.madmax@gmail.com>
2023-09-21 18:59:59 -0300

    fix : Contact class

--- module00/ex01/Contact.cpp
+++ module00/ex01/Contact.cpp
@@ -6,17 +6,159 @@
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 19:17:49 by mdouglas          #+#    #+#             */
-/*   Updated: 2023/09/21 14:18:06 by madmax42         ###   ########.fr       */
+/*   Updated: 2023/09/21 18:58:53 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-class Contact;
+Contact::Contact()
+{
+};
 
-Contact::Contact(
-	const std::string &firstName, const std::string &lastName, const std::string &nickname,
-	const std::string &phoneNumber, const std::string &darkestSecret
-) : firstName(firstName), lastName(lastName), nickname(nickname),
-	phoneNumber(phoneNumber), darkestSecret(darkestSecret)
-{};
+Contact::~Contact()
+{
+};
+
+// getters
+
+std::string Contact::get_first_name() const
+{
+	return (this->_first_name);
+};
+
+std::string Contact::get_last_name() const
+{
+	return (this->_last_name);
+};
+
+std::string Contact::get_nickname() const
+{
+	return (this->_nickname);
+};
+
+std::string Contact::get_phone_number() const
+{
+	return (this->_phone_number);
+};
+
+std::string Contact::get_darkest_secret() const
+{
+	return (this->_darkest_secret);
+};
+
+// setters
+
+/// @brief The function sets the first name of the contact.
+/// @param first_name The first name of the contact.
+/// @return int Returns 0 if the first name was set successfully, 1 otherwise.
+int	Contact::set_first_name(std::string first_name)
+{
+	if (first_name == "")
+		return (1);
+	for (size_t i = 0; i < first_name.size(); i++)
+	{
+		if (!isalpha(first_name[i]))
+		{
+			if (isspace(first_name[i]))
+				continue;
+			else
+				return (1);
+		}
+	}
+	if (first_name.length() > 10)
+	{
+		first_name[9] = '.';
+		first_name.erase(10, first_name.length() - 10);
+	}
+	this->_first_name = first_name;
+	return (0);
+};
+
+/// @brief The function sets the last name of the contact.
+/// @param last_name The last name of the contact.
+/// @return int Returns 0 if the last name was set successfully, 1 otherwise.
+int Contact::set_last_name(std::string last_name)
+{
+	if (last_name == "")
+		return (1);
+	for (size_t i = 0; i < last_name.size(); i++)
+	{
+		if (!isalpha(last_name[i]))
+		{
+			if (isspace(last_name[i]))
+				continue;
+			else
+				return (1);
+		}
+	}
+	this->_last_name = last_name;
+	return (0);
+};
+
+
+/// @brief The function sets the nickname of the contact.
+/// @param nickname The nickname of the contact.
+/// @return int Returns 0 if the nickname was set successfully, 1 otherwise.
+int Contact::set_nickname(std::string nickname)
+{
+	if (nickname == "")
+		return (1);
+	for (size_t i = 0; i < nickname.size(); i++)
+	{
+		if (!isalpha(nickname[i]))
+		{
+			if (isspace(nickname[i]))
+				continue;
+			else
+				return (1);
+		}
+	}
+	if (nickname.length() > 10)
+	{
+		nickname[9] = '.';
+		nickname.erase(10, nickname.length() - 10);
+	}
+	this->_nickname = nickname;
+	return (0);
+};
+
+/// @brief The function sets the phone number of the contact.
+/// @param phone_number The phone number of the contact.
+/// @return int Returns 0 if the phone number was set successfully, 1 otherwise.
+int	Contact::set_phone_number(std::string phone_number)
+{
+	if (phone_number == "")
+		return (1);
+	for (size_t i = 0; i < phone_number.size(); i++)
+	{
+		if (!isdigit(phone_number[i]))
+		{
+			if (isspace(phone_number[i]))
+				return (1);
+		}
+	}
+	if (phone_number.length() > 10)
+	{
+		phone_number[9] = '.';
+		phone_number.erase(10, phone_number.length() - 10);
+	}
+	this->_phone_number = phone_number;
+	return (0);
+};
+
+/// @brief This function sets the darkest secret of the contact.
+/// @param darkest_secret The darket secret of the contact.
+/// @return int Returns 0 if the darkest secret was set successfully, 1 otherwise.
+int Contact::set_darkest_secret(std::string darkest_secret)
+{
+	if (darkest_secret == "")
+		return (1);
+	if (darkest_secret.length() > 10)
+	{
+		darkest_secret[9] = '.';
+		darkest_secret.erase(10, darkest_secret.length() - 10);
+	}
+	this->_darkest_secret = darkest_secret;
+	return (0);
+};
--- module00/ex01/Contact.hpp
+++ module00/ex01/Contact.hpp
@@ -6,39 +6,43 @@
 /*   By: madmax42 <madmax42@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 19:28:31 by mdouglas          #+#    #+#             */
-/*   Updated: 2023/09/21 14:15:23 by madmax42         ###   ########.fr       */
+/*   Updated: 2023/09/21 18:48:53 by madmax42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
+# include <iostream>
 # include <string>
+# include <cstring>
+# include <iomanip>
 
-class Contact;
 
 class Contact
 {
-	public:
-		Contact (
-			const std::string& firstName, const std::string& lastName, const std::string& nickname,
-			const std::string& phoneNumber, const std::string& darkestSecret
-		);
-		Contact (){}
-	
-	// methods
-	std::string getFirstName() const { return firstName; }
-	std::string getLastName() const { return lastName; }
-	std::string getNickname() const { return nickname; }
-	std::string getPhoneNumber() const { return phoneNumber; }
-	std::string getDarkestSecret() const { return darkestSecret; }	
+public:
+	Contact();
+	~Contact();
 
-	private:
-		std::string firstName;
-		std::string lastName;
-		std::string nickname;
-		std::string phoneNumber;
-		std::string darkestSecret;
+	std::string get_first_name() const;
+	std::string get_last_name() const;
+	std::string get_nickname() const;
+	std::string get_phone_number() const;
+	std::string get_darkest_secret() const;
+	
+	int	set_first_name(std::string first_name);
+	int	set_last_name(std::string last_name);
+	int set_nickname(std::string nickname);
+	int set_phone_number(std::string phone_number);
+	int set_darkest_secret(std::string darkest_secret);
+
+private:
+	std::string _first_name;
+	std::string _last_name;
+	std::string _nickname;
+	std::string _phone_number;
+	std::string _darkest_secret;
 };
 
 #endif
